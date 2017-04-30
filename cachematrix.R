27bdfd32c2a63e0af0e17651306d9aeeaa71c6eb
cj7524 <colin.johnson@outlook.com>
2017-04-30 11:04:52 -0600

    Update with my own code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions when used together will check to see if there is a cached version 
+## of a matrix that we are trying to create an inverse of.  If there isn't, it will
+## run the inverse.  If there is it will call the cached version of the inverse.
+## These two functions will only work with invertible matrices.  Use det(x) to 
+## determine if a matrix is invertible (0 is no)
 
-## Write a short comment describing this function
+##This reads in a matrix and creates a cached version of an inversed matrix by
+##putting it into a list
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      inv <- NULL
+      set <- function(y) {
+            x <<- y
+            inv <<- NULL
+      }
+      get <- function() x
+      setInverse <- function(inversed_M) inv <<- inversed_M
+      getInverse <- function() inv
+      list(set = set, get = get,
+           setInverse = setInverse,
+           getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+##Determine if cache matrix created or if need to create an inverse
+## if there is no cache matrix then it will create one via solve
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      
+      
+      inv <- x$getInverse()
+      if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+      }
+      data <- x$get()
+      inv <- solve(data, ...)
+      x$setmInverse(inv)
+      return(inv) 
+      ## Return a matrix that is the inverse of 'x'
 }
